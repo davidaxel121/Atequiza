0c3628b2cd6d98f97e6ab6f4981fcb24b7021db9
davidaxel121 <davidaxel121@gmail.com>
2020-07-16 13:13:18 -0500

    Adding masonry section and hero section

--- react/src/pages/TecMM/Tecmm.js
+++ react/src/pages/TecMM/Tecmm.js
@@ -2,15 +2,84 @@ import React from 'react';
 // import OnProgress from '../../components/OnProgress/OnProgress'
 import Navbartec from './Navbartec'
 import './Tecmm.css';
+
+import landing from '../../img/landing.jpg';
+import tshirt from '../../img/tshirt.jpg';
+import house from '../../img/house.jpg';
 // import Navbar from '../../components/Navbar/Navbar';
 
 class Tecmm extends React.Component {
 
     render() {
         return (
-            <div>
-                <Navbartec></Navbartec>
-            <h1>Hola Pagina</h1>
+            <div className="tecmm-page">
+
+                <span className="fixed-top"><Navbartec></Navbartec></span>
+
+                <section className="d-flex justify-content-center align-items-center container-fluid text-center section-hero" id="section-hero">
+                    <h1>Vida estudiantil en el Tecmm Campus Chapala</h1>
+                </section>
+
+                <div className="container">
+
+                    <section className="" id="section-events">
+                        <div class="masonry">
+                            <div class="item level-1">
+                                <img src={landing} alt=""></img>
+                            </div>
+                            <div class="item level-2">
+                                <img src={tshirt} alt=""></img>
+                            </div>
+                            <div class="item level-3">
+                                <img src={house} alt=""></img>
+                            </div>
+                            <div class="item level-1">
+                                <img src={landing} alt=""></img>
+                            </div>
+                            <div class="item level-2">
+                                <img src={tshirt} alt=""></img>
+                            </div>
+                            <div class="item level-3">
+                                <img src={house} alt=""></img>
+                            </div>
+                            <div class="item level-1">
+                                <img src={landing} alt=""></img>
+                            </div>
+                            <div class="item level-2">
+                                <img src={tshirt} alt=""></img>
+                            </div>
+                            <div class="item level-3">
+                                <img src={house} alt=""></img>
+                            </div>
+                            <div class="item level-1">
+                                <img src={landing} alt=""></img>
+                            </div>
+                            <div class="item level-2">
+                                <img src={tshirt} alt=""></img>
+                            </div>
+                            <div class="item level-3">
+                                <img src={house} alt=""></img>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section className="d-flex justify-content-center align-items-center container-fluid text-center section-hero" id="section-hero">
+                        <h1>Vida estudiantil en el Tecmm Campus Chapala</h1>
+                    </section>
+
+                    <section className="d-flex justify-content-center align-items-center container-fluid text-center section-hero" id="section-hero">
+                        <h1>Vida estudiantil en el Tecmm Campus Chapala</h1>
+                    </section>
+
+                    <section className="d-flex justify-content-center align-items-center container-fluid text-center section-hero" id="section-hero">
+                        <h1>Vida estudiantil en el Tecmm Campus Chapala</h1>
+                    </section>
+
+                    <footer className="d-flex justify-content-center align-items-center container-fluid text-center section-hero" id="section-hero">
+                        <h1>Vida estudiantil en el Tecmm Campus Chapala</h1>
+                    </footer>
+
+                </div>
             </div>
         );
     }
