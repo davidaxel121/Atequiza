e4b5e964feeb10a4dd509913ebfaaefba0e12dc7
davidaxel121 <davidaxel121@gmail.com>
2020-07-19 14:10:21 -0500

    fixing styles on navbar and announcement section
    Ready :100:

--- react/src/pages/TecMM/Tecmm.js
+++ react/src/pages/TecMM/Tecmm.js
@@ -8,7 +8,7 @@ import tshirt from '../../img/tshirt.jpg';
 import house from '../../img/house.jpg';
 import exampleimage1 from '../../img/example-1.jpg';
 import exampleimage2 from '../../img/example-2.png';
-import {Navbar, Carousel, Nav } from 'react-bootstrap'; 
+import { Navbar, Carousel, Nav } from 'react-bootstrap';
 // import Navbar from '../../components/Navbar/Navbar';
 
 class Tecmm extends React.Component {
@@ -19,11 +19,11 @@ class Tecmm extends React.Component {
 
                 {/* <span className="fixed-top"><Navbartec></Navbartec></span> */}
 
-                <Navbar className="justify-content-around" fixed="top" expand="lg">
+                <Navbar className="" fixed="top" expand="lg">
                     <Navbar.Brand href="#home">Tecmm Campus Chapala</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
+                        <Nav className="ml-auto text-center">
                             <Nav.Link href="#section-hero">Vida estudiantil</Nav.Link>
                             <Nav.Link href="#section-events">Eventos</Nav.Link>
                             <Nav.Link href="#section-announcement">Anuncios</Nav.Link>
@@ -39,7 +39,7 @@ class Tecmm extends React.Component {
                 <div className="container">
 
                     <section className="section section-events text-center" id="section-events">
-                        <h3>Eventos anteriores</h3>
+                        <h3 className="title-section">Eventos anteriores</h3>
 
                         <div className="masonry">
                             <div className="item level-1">
@@ -82,14 +82,14 @@ class Tecmm extends React.Component {
                     </section>
 
                     <section className="section section-announcement text-center" id="section-announcement">
-                        <h3>Anuncios</h3>
+                        <h3 className="title-section">Anuncios</h3>
 
                         <div>
                             <h4 className="important">Importante</h4>
                             <img src={exampleimage2} className="img-fluid" alt="Important annoucement" />
                         </div>
 
-                        <br/>
+                        <br />
 
                         <div>
                             <Carousel>
@@ -100,7 +100,7 @@ class Tecmm extends React.Component {
                                         alt="First slide"
                                     />
                                     <Carousel.Caption>
-                                        <h3>Primer enlace</h3>
+                                        <h3>Primer anuncio</h3>
                                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                                     </Carousel.Caption>
                                 </Carousel.Item>
@@ -112,7 +112,7 @@ class Tecmm extends React.Component {
                                     />
 
                                     <Carousel.Caption>
-                                        <h3>Segundo enlace</h3>
+                                        <h3>Segundo anuncio</h3>
                                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                                     </Carousel.Caption>
                                 </Carousel.Item>
@@ -124,7 +124,7 @@ class Tecmm extends React.Component {
                                     />
 
                                     <Carousel.Caption>
-                                        <h3>Tercer enlace</h3>
+                                        <h3>Tercer anuncio</h3>
                                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                                     </Carousel.Caption>
                                 </Carousel.Item>
@@ -137,30 +137,46 @@ class Tecmm extends React.Component {
                         <div className="row">
                             <div className="col">
 
-                                <h3>Enlaces importantes</h3>
+                                <h3 className="title-section">Enlaces importantes</h3>
 
                                 <div className="row">
-                                    <div className="col-12 col-md-6 order-1 order-md-1"><img src={exampleimage2} className="img-fluid" alt="Link 1" /></div>
-                                    <div className="col-12 col-md-6 order-2 order-md-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores dolorem maxime laboriosam magnam expedita architecto omnis aut impedit? Enim amet, aliquid facilis possimus quis similique repellendus doloribus! Illum, nisi consequatur! Blanditiis nihil, amet debitis alias minima mollitia, molestiae, recusandae quasi vitae quidem officiis illo culpa omnis earum tenetur magnam architecto. Nulla tempore dolorem quidem, excepturi voluptatem voluptate at commodi in debitis? Quasi, modi. Magnam impedit, beatae aspernatur maiores vero dicta!</div>
+                                    <div className="col-12 col-md-6 order-1 order-md-1 section-links-images d-flex justify-content-center align-items-center"><img src={exampleimage2} className="" alt="Link 1" /></div>
+                                    <div className="col-12 col-md-6 order-2 order-md-2 link-color-1 section-links-text">
+
+                                        <h5 className="link-title">EDCORE</h5>
+
+                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro aut sunt dignissimos ex hic nam, sed facere possimus quibusdam repellendus perspiciatis animi rerum soluta nesciunt nemo. Dolorem nemo modi ad suscipit quasi sint consectetur itaque magnam sequi enim magni quae deserunt, cumque, necessitatibus molestiae reprehenderit dignissimos quaerat maxime obcaecati quis aut dicta. Earum, explicabo ut? Repellendus tenetur aut ea, cum voluptate eius quidem ipsa dolorum maiores, quos ipsam, hic laborum.</div>
                                 </div>
                                 <div className="row">
-                                    <div className="col-12 col-md-6 order-2 order-md-1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores dolorem maxime laboriosam magnam expedita architecto omnis aut impedit? Enim amet, aliquid facilis possimus quis similique repellendus doloribus! Illum, nisi consequatur! Blanditiis nihil, amet debitis alias minima mollitia, molestiae, recusandae quasi vitae quidem officiis illo culpa omnis earum tenetur magnam architecto. Nulla tempore dolorem quidem, excepturi voluptatem voluptate at commodi in debitis? Quasi, modi. Magnam impedit, beatae aspernatur maiores vero dicta!</div>
-                                    <div className="col-12 col-md-6 order-1 order-md-2"><img src={exampleimage2} className="img-fluid" alt="Link 1" /></div>
+                                    <div className="col-12 col-md-6 order-2 order-md-1 link-color-2 section-links-text">
+
+                                        <h5 className="link-title">EDCORE</h5>
+
+                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro aut sunt dignissimos ex hic nam, sed facere possimus quibusdam repellendus perspiciatis animi rerum soluta nesciunt nemo. Dolorem nemo modi ad suscipit quasi sint consectetur itaque magnam sequi enim magni quae deserunt, cumque, necessitatibus molestiae reprehenderit dignissimos quaerat maxime obcaecati quis aut dicta. Earum, explicabo ut? Repellendus tenetur aut ea, cum voluptate eius quidem ipsa dolorum maiores, quos ipsam, hic laborum.</div>
+                                    <div className="col-12 col-md-6 order-1 order-md-2 section-links-images d-flex justify-content-center align-items-center"><img src={exampleimage2} className="" alt="Link 1" /></div>
                                 </div>
                                 <div className="row">
-                                    <div className="col-12 col-md-6 order-1 order-md-1"><img src={exampleimage2} className="img-fluid" alt="Link 1" /></div>
-                                    <div className="col-12 col-md-6 order-2 order-md-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores dolorem maxime laboriosam magnam expedita architecto omnis aut impedit? Enim amet, aliquid facilis possimus quis similique repellendus doloribus! Illum, nisi consequatur! Blanditiis nihil, amet debitis alias minima mollitia, molestiae, recusandae quasi vitae quidem officiis illo culpa omnis earum tenetur magnam architecto. Nulla tempore dolorem quidem, excepturi voluptatem voluptate at commodi in debitis? Quasi, modi. Magnam impedit, beatae aspernatur maiores vero dicta!</div>
+                                    <div className="col-12 col-md-6 order-1 order-md-1 section-links-images d-flex justify-content-center align-items-center"><img src={exampleimage2} className="" alt="Link 1" /></div>
+                                    <div className="col-12 col-md-6 order-2 order-md-2 link-color-3 section-links-text">
+
+                                        <h5 className="link-title">EDCORE</h5>
+
+                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro aut sunt dignissimos ex hic nam, sed facere possimus quibusdam repellendus perspiciatis animi rerum soluta nesciunt nemo. Dolorem nemo modi ad suscipit quasi sint consectetur itaque magnam sequi enim magni quae deserunt, cumque, necessitatibus molestiae reprehenderit dignissimos quaerat maxime obcaecati quis aut dicta. Earum, explicabo ut? Repellendus tenetur aut ea, cum voluptate eius quidem ipsa dolorum maiores, quos ipsam, hic laborum.</div>
                                 </div>
                                 <div className="row">
-                                    <div className="col-12 col-md-6 order-2 order-md-1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores dolorem maxime laboriosam magnam expedita architecto omnis aut impedit? Enim amet, aliquid facilis possimus quis similique repellendus doloribus! Illum, nisi consequatur! Blanditiis nihil, amet debitis alias minima mollitia, molestiae, recusandae quasi vitae quidem officiis illo culpa omnis earum tenetur magnam architecto. Nulla tempore dolorem quidem, excepturi voluptatem voluptate at commodi in debitis? Quasi, modi. Magnam impedit, beatae aspernatur maiores vero dicta!</div>
-                                    <div className="col-12 col-md-6 order-1 order-md-2"><img src={exampleimage2} className="img-fluid" alt="Link 1" /></div>
+                                    <div className="col-12 col-md-6 order-2 order-md-1 link-color-1 section-links-text">
+
+                                        <h5 className="link-title">EDCORE</h5>
+
+                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro aut sunt dignissimos ex hic nam, sed facere possimus quibusdam repellendus perspiciatis animi rerum soluta nesciunt nemo. Dolorem nemo modi ad suscipit quasi sint consectetur itaque magnam sequi enim magni quae deserunt, cumque, necessitatibus molestiae reprehenderit dignissimos quaerat maxime obcaecati quis aut dicta. Earum, explicabo ut? Repellendus tenetur aut ea, cum voluptate eius quidem ipsa dolorum maiores, quos ipsam, hic laborum.</div>
+                                    <div className="col-12 col-md-6 order-1 order-md-2 section-links-images d-flex justify-content-center align-items-center"><img src={exampleimage2} className="" alt="Link 1" /></div>
                                 </div>
                             </div>
                         </div>
                     </section>
 
-                    <footer className="footer text-center" id="footer">
-                        <hr/>
+                    <footer className="section footer text-center" id="footer">
+                        <hr />
                         <h6>Tecmm Chapala 2020 - 2024</h6>
                     </footer>
 
